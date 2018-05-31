63d19426994a9f0b784423c5b8ac34a34b6ef49c
Angular CLI <angular-cli@angular.io>
2018-05-31 14:33:35 +0530

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { ShoppingcartappPage } from './app.po';
+
+describe('shoppingcartapp App', () => {
+  let page: ShoppingcartappPage;
+
+  beforeEach(() => {
+    page = new ShoppingcartappPage();
+  });
+
+  it('should display welcome message', done => {
+    page.navigateTo();
+    page.getParagraphText()
+      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(done, done.fail);
+  });
+});
